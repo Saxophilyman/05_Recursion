2f7d578ea03d82b1d1cddce802ed6a107f030bba
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-09-07 17:22:58 +0400

    Update 03_task.java

--- 03_task.java
+++ 03_task.java
@@ -1,3 +1,14 @@
 //5. печать только чётных значений из списка;
+    public static void printEvenValuesFromList(List<Integer> arrayList) {
+        if (arrayList.isEmpty()) {
+            return;
+        }
+        if (arrayList.get(0) % 2 == 0) {
+            System.out.println(arrayList.get(0));
+        }
+        arrayList.remove(0); // можно удалить 9 и 10 строку и вместо них использовать: printEvenValuesFromList(arrayList.subList(1, arrayList.size()));
+        printEvenValuesFromList(arrayList);
+    }
+
 
 //6. печать элементов списка с чётными индексами;
